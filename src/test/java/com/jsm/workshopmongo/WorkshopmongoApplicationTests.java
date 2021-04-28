fa950794742952da2fbd267e68959e4a677fd9ba
Jeffrei S Moreira <jeffreism@gmail.com>
2021-04-28 14:37:38 -0300

    Primeiro commit

---
+++ src/test/java/com/jsm/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsm.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
